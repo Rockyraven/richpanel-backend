642baa4f8e7cdaca55df342d44af3bdf79e5f1c4
rockyraven <rocky23012001@gmail.com>
2024-02-22 14:28:54 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const messageRouter = require('./routes/messageRouter');
+const express = require("express");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const messageRouter = require("./routes/messageRouter");
 const mongoose = require("mongoose");
+const http = require("http");
+const socketIo = require("socket.io");
+const messageModel = require("./modal/messageModal");
 require("dotenv").config();
 const app = express();
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
 app.use(bodyParser.json());
-app.use('/', messageRouter);
+app.use("/", messageRouter);
 
 app.get("/", (req, res) => {
-  res.send("hello world")
-})
+  res.send("hello world");
+});
 
-
-console.log(process.env.PORT, "with port number working")
+console.log(process.env.PORT, "with port number working");
 
 mongoose
-  .connect(
-   process.env.MONGO_URL
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("database connected ");
   })
@@ -27,7 +30,23 @@ mongoose
     console.log(error);
   });
 
+io.on("connection", (socket) => {
+  console.log("A client connected");
 
+  // Send messages to client whenever there's a new message
+  socket.on("newMessage", async () => {
+    try {
+      const messages = await messageModel.find(); // Fetch messages from the database
+      io.emit("messages", messages); // Emit messages to all connected clients
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
